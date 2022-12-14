785199f85a5fa67189ea9004ee3e68a55a02eb1c
DangTranTuanAnh <dangtrantuananh.2002@gmail.com>
2022-12-14 15:05:03 +0700

    create CRUD

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,7 +1,9 @@
 
 import db from '../models/index';
+import CRUDService from '../services/CRUDService';
+
 let getHomePage = async (req, res) => {
-    try {    
+    try {
         let data = await db.User.findAll();
         return res.render('homepage.ejs', {
             data: JSON.stringify(data)
@@ -14,7 +16,72 @@ let getAboutPage = (req, res) => {
     return res.render('test/about.ejs');
 }
 
+let getCRUD = (req, res) => {
+    return res.render('crud.ejs');
+}
+
+let postCRUD = async (req, res) => {
+    let message = await CRUDService.createNewUser(req.body);
+    console.log(message);
+    return res.send('post crud from server');
+}
+
+let displayGetCRUD = async (req, res) => {
+    let data = await CRUDService.getAllUser();
+    // console.log('------------------------');
+    // console.log(data);
+    // console.log('--------------------------');
+    return res.render('displayCRUD.ejs', {
+        dataTable: data
+    })
+}
+
+let getEditCRUD = async (req, res) => {
+    let userId = req.query.id;
+    if (userId) {
+        let userData = await CRUDService.getUserInforById(userId);
+        // console.log('----------------------')
+        // console.log(userData)
+        // console.log('----------------------')
+
+        //check userData not found
+
+
+        // let userData
+
+        return res.render('editCRUD.ejs', {
+            user: userData
+        });
+    } else {
+        return res.send('Users not found');
+    }
+}
+
+let putCRUD = async (req, res) => {
+    let data = req.body;
+    let allUsers = await CRUDService.updateUserData(data);
+    return res.render('displayCRUD.ejs', {
+        dataTable: allUsers
+    });
+}
+
+let deleteCRUD = async (req, res) => {
+    let id = req.query.id;
+    if (id) {
+        await CRUDService.deleteUserById(id);
+        return res.send('delete the user succeed')
+    } else {
+        return res.send('User not found')
+    }
+}
+
 module.exports = {
-    getHomePage : getHomePage,
-    getAboutPage : getAboutPage,
+    getHomePage: getHomePage,
+    getAboutPage: getAboutPage,
+    getCRUD: getCRUD,
+    postCRUD: postCRUD,
+    displayGetCRUD: displayGetCRUD,
+    getEditCRUD: getEditCRUD,
+    putCRUD: putCRUD,
+    deleteCRUD: deleteCRUD,
 }
